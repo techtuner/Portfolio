cf3455a57ec23aef708ab68d89dfbec204ef7bd9
techtuner <techtuner284@gmail.com>
2021-03-02 21:23:32 +0530

    mobile first commit

--- js/about.js
+++ js/about.js
@@ -1,11 +1,3 @@
-//Typed Js
-var typed = new Typed(".typing", {
-  strings: ["Web Developer", "Gamer", "Designer"],
-  typeSpeed: 50,
-  backSpeed: 60,
-  loop: true,
-});
-//Navbar Styling
 const navSlide = () => {
   const burger = document.querySelector(".hamburger");
   const nav = document.querySelector(".nav-links");
